262886dd4cbf8c21c799675eb2573709eb786427
Darrell <darrell78383@hotmail.com>
2021-11-12 00:47:47 +1100

    Experimenting with different libraries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/LoginForm';
+import Register from './components/RegisterForm';
+import NavBar from './components/NavBar';
 
-function App () {
+export default function App () {
   return (
-    <></>
+    <Router>
+      <NavBar />
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/Login">Login</Link>
+      </div>
+      <div>
+        <Link to="/Register">Register</Link>
+      </div>
+
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route exact path="/Login" element={<Login />}/>
+        <Route exact path="/Register" element={<Register />}/>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Nav, Container, Navbar } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function NavBar () {
+  return (<>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+  </>);
+}
+
+export default NavBar;
