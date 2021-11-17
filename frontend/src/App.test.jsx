e2907c24df893eb609a4d0d589e517f4a975a97a
Darrell <darrell78383@hotmail.com>
2021-11-18 10:10:41 +1100

    Fix

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
+import { shallow } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('NavBar', () => {
+  const noop = () => {};
+
+  // render(<NavBar />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+
+  it('contains AirBrb (home), login, register links', () => {
+    let isTokenEmpty = false;
+    const setIsTokenEmpty = (bool) => {
+      isTokenEmpty = bool;
+    }
+    const navBar = shallow(
+    <Router>
+      <NavBar setIsTokenEmpty={setIsTokenEmpty} />
+    </Router>
+    );
+    
+    console.log(navBar.children().getElement().text());
+  });
+
+  // it('after logging in or registering, contains logout link', () => {
+    
+  // });
+
+  // it('always present, during login/register', () => {
+    
+  // });
+
+  // it('triggers onClick event handler when AirBrb is clicked', () => {
+    
+  // });
+
+  // it('triggers onClick event handler when login is clicked', () => {
+    
+  // });
+
+  // it('triggers onClick event handler when register is clicked', () => {
+    
+  // });
+
+  // it('triggers onClick event handler when logout is clicked', () => {
+    
+  // });
+  
 });
